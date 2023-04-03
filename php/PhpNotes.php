595af328989168235f914f77d4d96197b3b3cc97
Sohaib-Mehmood <sohaibmehmood776@gmail.com>
2023-04-03 20:28:36 +0500

    added php files

---
+++ php/PhpNotes.php
@@ -0,0 +1,12 @@
+installed:
+    - Home brew
+    - curl
+    - php
+    - mysql
+php help:
+    - php -h
+
+run the html file using the followning commands:
+    - php -S localhost:8888
+Points:
+    - to get the full power of the PHP, we shoould be creating the index.php file instead of index.html file.
