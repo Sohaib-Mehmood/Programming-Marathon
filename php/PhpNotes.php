39a70ba231b10c153e110de80b2b1eb341b80322
Sohaib-Mehmood <sohaibmehmood776@gmail.com>
2023-04-06 20:19:17 +0500

    updated php notes

--- php/PhpNotes.php
+++ php/PhpNotes.php
@@ -10,3 +10,9 @@ run the html file using the followning commands:
     - php -S localhost:8888
 Points:
     - to get the full power of the PHP, we shoould be creating the index.php file instead of index.html file.
+
+variables:
+    $varName;
+        - example;
+            $yourName = "sohaib".
+code refactoring is done through variable, because of the reusability.
